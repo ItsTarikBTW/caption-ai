5433b29f570981851f5dd721682fa5323255d0ee
tarik <77856978+ItsTarikBTW@users.noreply.github.com>
2023-09-19 23:00:32 +0100

    export fake data

---
+++ src/data/chars.tsx
@@ -0,0 +1,115 @@
+
+export const data1 = [
+    {
+      revenue: 10400,
+      subscription: 240,
+    },
+    {
+      revenue: 14405,
+      subscription: 300,
+    },
+    {
+      revenue: 9400,
+      subscription: 200,
+    },
+    {
+      revenue: 8200,
+      subscription: 278,
+    },
+    {
+      revenue: 7000,
+      subscription: 189,
+    },
+    {
+      revenue: 9600,
+      subscription: 239,
+    },
+    {
+      revenue: 11244,
+      subscription: 278,
+    },
+    {
+      revenue: 26475,
+      subscription: 189,
+    },
+  ];
+  export const data2 = [
+    {
+      average: 400,
+      today: 240,
+    },
+    {
+      average: 300,
+      today: 139,
+    },
+    {
+      average: 200,
+      today: 980,
+    },
+    {
+      average: 278,
+      today: 390,
+    },
+    {
+      average: 189,
+      today: 480,
+    },
+    {
+      average: 239,
+      today: 380,
+    },
+    {
+      average: 349,
+      today: 430,
+    },
+  ];
+  export const data3 = [
+    {
+      name: "Jan",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Feb",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Mar",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Apr",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "May",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Jun",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Jul",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Aug",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Sep",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Oct",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Nov",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+    {
+      name: "Dec",
+      total: Math.floor(Math.random() * 5000) + 1000,
+    },
+  ];
