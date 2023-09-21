92166a5234ab3c7dd0a1c1de16e63011a4216466
tarik <77856978+ItsTarikBTW@users.noreply.github.com>
2023-09-21 10:49:28 +0100

    show user table if screen < md

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -29,9 +29,6 @@ export default async function UserPage() {
 
   return (
     <Admin>
-      <div className="md:hidden">
-        screen to small 😒🤏
-      </div>
       <div className="hidden flex-1 flex-col space-y-8 p-8 md:flex">
         <div className="flex items-center justify-between space-y-2">
           <div>
