7fa7b2d0ebb020c10a4673ab487e76931e97e7d8
nadir <nadirmehtaripr@gmail.com>
2023-10-05 10:49:14 +0100

    add hero screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import Navbar from './components/landing-page/Navbar'
+import HeroMobile from './components/landing-page/HeroMobile'
+import Hero from './components/landing-page/Hero'
 
 export default function page() {
   return (
-    <main>
+    <main className='bg-white'>
       <Navbar /> 
+      <HeroMobile />
+      <Hero />
     </main>
   )
 }
